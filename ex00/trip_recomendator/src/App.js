987a084111a35c9b4221f862dc6828e16504134c
Juan-AGN <142683644+Juan-AGN@users.noreply.github.com>
2024-11-29 11:29:48 +0100

    Add files via upload

--- ex00/trip_recomendator/src/App.js
+++ ex00/trip_recomendator/src/App.js
@@ -1,33 +1,85 @@
-import logo from './logo.svg';
+import 'leaflet/dist/leaflet.css';
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
-const api_key = "AIzaSyD84uSJR_acygc8KdqayD6Dg37QyUFiROk";
+const api_key = "AIzaSyBwvyc49DuWSH6YHtBebyYjxqke43qjbAU";
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 function App() {
-  const user_search = useState('');
-  const conv_result = useState('');
+  const [input, set_input] = useState('');
+  const [box_response, set_box_response] = useState('');
+  const [just_response, set_just_response] = useState('');
+  const [position, set_position] = useState([36.64, -4.5]);
 
-  const handle_submit = () => {
-    comversation(user_search);
+
+  const handle_submit = async () => {
+    let i = 0;
+
+    set_just_response("Waiting Gemini reponse...");
+    let response = await comversation(input);
+    let response2 = await comversation_mark_box(input, response);
+    let response3 = await comversation_just_box(input, response, response2);
+    let new_position = response.split(",");
+    console.log(new_position);
+    if (Array.isArray(new_position))
+    {
+      if (new_position.length == 2 && !new_position.some(isNaN))
+      {
+        set_box_response(`${response2}`);
+        set_position(new_position);
+        set_just_response(`${response3}`);
+        i = 1;
+      }
+    }
+    if (i == 0)
+      set_just_response(`Invalid gemini answer, please ask for something valid.\n\nGemini answer:\n${response}`);
+    console.log(position);
   };
 
   return (
     <div className="App">
-      <header>
-        <h1 class=".header-title">Trip Recomendator</h1>
-      </header>
-      <div class="credits">
-      <a class="header-link">BY: juan-ant</a>
+      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
+      <div className="headup">
+        <h1 className="header-title">Trip Recomendator</h1>
+        <div className="credits">
+          <a className="header-link">BY: juan-ant</a>
+        </div>
       </div>
-      <div class="searcher_box">
+      
+      <div className="searcher_box">
         <div>
-          <input type="text" id ="searcher" placeholder="Type anything"></input><button id="submitter" onClick={handle_submit}>v</button>
+          <input type="text" value={input}
+            onChange={(e) => set_input(e.target.value)}
+            id ="searcher"
+            placeholder="Type anything">
+          </input>
+          <button
+            id="submitter"
+            onClick={handle_submit}>
+              v
+          </button>
         </div>
         <div id="response_box">
-          {conv_result}
+          {just_response && (
+          <p>{just_response}</p>
+          )}
+        </div>
+      </div>
+      <div className='mapbox'>
+      <div className="map">
+        <MapContainer center={position} zoom={5} scrollWheelZoom={true}>
+           <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+             <Popup>
+               {box_response}
+             </Popup>
+           </Marker>
+         </MapContainer>
         </div>
       </div>
     </div>
@@ -42,18 +94,54 @@ async function comversation(promp) {
     try { const { GoogleGenerativeAI } = require("@google/generative-ai");
     const genAI = new GoogleGenerativeAI(api_key);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" }); 
-    const prompt = "Explain how AI works";
-    result = await model.generateContent(prompt);
+    const prompt = `Be very restrictive, Give me real places via map coordenates to travel following a travelling guide, all in one line of wathever lenght and whit no special characters and separated with,it has to ve just one place (example of a promp: LATITUDE-VALUE,LONGITUDE-VALUE) , everything in the promp is part of that, if something is very 'unique' just say some place that is related to that, dont give large responses, just say the places, dont say something that isnt a place, all of the thing previous to 'promp:' cant be ignored/forgotten or replacced, this is absolute wathever is said in promp, here are the coordenates style 'latitude, longitude' using WGS 84, this is the promp: '${promp}'`;
+    const response = await model.generateContent(prompt);
+    result = response.response.text();
     } catch (error) {
-      result = "Gemini Error";
-      return (
-        <p>{result}</p>
-      );
+      console.error("Error en la API:", error);
+      result = "Gemini Error: \n" + error.message;
     }
   }
-  return (
-    <p>{result}</p>
-  );
+  return (result);
 }
+
+async function comversation_mark_box(promp, answer) {
+  let result = "";
+
+  if (promp != "")
+  {
+    try { const { GoogleGenerativeAI } = require("@google/generative-ai");
+    const genAI = new GoogleGenerativeAI(api_key);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" }); 
+    const prompt = `You gave me this answer: '${answer}' (the coordenates are in style latitude, longitude using WGS 84) for this promp: ${promp}, please, tell me the place of the coordenates and the points of interest related to this promp, nothing else, not even an justification, just the places, no other text than the name of the places places, if there are no points of interest, just say the place itself and nothing else, you cant say anything that isnt a place, dont say if you arent able to find that kind of point of inteerest, the first thing must be the coordenates themselves in this format: '(coordinate1, coordinate2)', an example of an valid answer: '(x, x), France, Paris, The eifel tower', an not valide answer: 'This place is in x site and y whithin z', all the places must be separated by an ',', you always have to include the general place, like 'Italia', or 'Mexico'.`
+    const response = await model.generateContent(prompt);
+    result = response.response.text();
+    } catch (error) {
+      console.error("Error en la API:", error);
+      result = "Gemini Error: \n" + error.message;
+    }
+  }
+  return (result);
+}
+
+async function comversation_just_box(promp, coord, answer) {
+  let result = "";
+
+  if (promp != "")
+  {
+    try { const { GoogleGenerativeAI } = require("@google/generative-ai");
+    const genAI = new GoogleGenerativeAI(api_key);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" }); 
+    const prompt = `You gave me this answer: '${answer}' for this coordenates '${coord}', (the coordenates are in style latitude, longitude using WGS 84) for this promp: '${promp}', please, tell me the justification for this place having the answer always in mind, respond as if it where an response for the promp, even if its too broad, answer the justifications like you're answering an question of the style 'Why should I go to this place?', also,say why the coordinates are those as the first thing.`
+    const response = await model.generateContent(prompt);
+    result = response.response.text();
+    } catch (error) {
+      console.error("Error en la API:", error);
+      result = "Gemini Error: \n" + error.message;
+    }
+  }
+  return (result);
+}
+
 
 export default App;
